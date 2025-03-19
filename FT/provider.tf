8dd0cdf0fcea0dd14a95be38a10d7b1311332061
hemalathasm <69075439+hemalathasm@users.noreply.github.com>
2025-03-19 10:31:11 +0530

    Update provider.tf

--- FT/provider.tf
+++ FT/provider.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    hcp = {
-      source = "hashicorp/hcp"
-      version = "~> 0.55.0"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "hcp" {
-  client_secret = var.hcp_client_secret
-}
-
 provider "aws" {
   region = var.region
 }
